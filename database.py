1517e3f87724b37fafa19af1b472965a4ea3e412
Akinwunmi <akinwunmiolusegun277@gmail.com>
2026-08-11 19:04:03 +0100

    chore: installed dependencies and created server and databse files

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLAALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+engine = create_engine(SQLAALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
---
+++ server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import Field, BaseModel
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def read_root(db: Session = Depends(get_db)):
+    return {"Hello": "World", "Database": db}
