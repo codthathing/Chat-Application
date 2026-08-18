e1908c9370855ea147ebb2687a0ad3ff24a519a7
Akinwunmi <akinwunmiolusegun277@gmail.com>
2026-08-18 14:31:54 +0100

    feat: created the database in sqllite

--- server.py
+++ server.py
@@ -7,6 +7,12 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
+class User(BaseModel):
+    username: str = Field(min_length=1, max_length=20)
+    email: str = Field(min_length=1, max_length=30)
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -14,6 +20,16 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/")
 def read_root(db: Session = Depends(get_db)):
-    return {"Hello": "World", "Database": db}
+    return {"users": db.query(models.User).all()}
+
+
+@app.post("/users")
+def upload_user(user: User, db: Session = Depends(get_db)):
+    user_model = models.User(username=user.username, email=user.email)
+    db.add(user_model)
+    db.commit()
+
+    return user
